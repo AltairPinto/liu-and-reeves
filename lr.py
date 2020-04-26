a2fac890eae486a85569ef03b5191fa22a6aa508
F. Emerson <f@staticagent.com>
2020-04-25 23:26:48 -0300

    lr: tragar algoritmo como selection sort, ordenando pi

--- lr.py
+++ lr.py
@@ -28,25 +28,24 @@ def weight(m, n, i, k):
 
 
 # Eq. 5: Tempo de processamento da tarefa artificial
-def artificial_processing_time(m, n, p, u, i, j):
+def artificial_processing_time(m, n, p, pi, k, i, j):
     p_a = 0.
-    for q in u:
+    for q in pi[k:n]:
         p_a += p[i][q]
-
     p_a -= p[i][j]
 
-    return p_a / (len(u) - 1)
+    return p_a / (n-k-1)
 
 
 # Eq. 11
-def index(m, n, p, pi, u, j):
+def index(m, n, p, pi, k, j):
     # Eq. 1: Tempo de conclusão
 
     # XXX(staticagent): As primeiras linha e coluna são inutilizadas, e estão
     # preenchidas com 0 para servir como dummies na comparação feita por max.
     # Existe um modo de utilizar esse conhecimento para diminuir ainda mais o
     # consumo de memória, mas, no momento, eu tenho que otimizar outras funções.
-    c = zeros((m+1, len(pi)+1))
+    c = zeros((m+1, k+1))
 
     for machine in range(1, c.shape[0]):
         for job in range(1, c.shape[1]):
@@ -71,25 +70,25 @@ def index(m, n, p, pi, u, j):
     it = 0.
     for i in range(1, m):
         idle_time = max(c_next[i] - c[i+1][-1], 0)
-        it += weight(m, n, i, len(pi)) * idle_time
+        it += weight(m, n, i, k) * idle_time
 
     # Eq. 8 e 9: Tempo de conclusão da tarefa artificial
     c_a = zeros((c.shape[0]))
     for machine in range(1, c_a.shape[0]):
         lhs = c_a[machine - 1]
         rhs = c_next[machine]
-        p_ia = artificial_processing_time(m, n, p, u, machine-1, j)
+        p_ia = artificial_processing_time(m, n, p, pi, k, machine-1, j)
 
         c_a[machine] = max(lhs, rhs) + p_ia
 
     # Eq. 10: Tempo total de conclusão artificial
     at = c_next[-1] + c_a[-1]
 
-    return (len(u) - 2)*it + at
+    return (n-k-2)*it + at
 
 
 def lr(m, n, p, x):
-    ranked = sorted(range(0, n), key=lambda j: index(m, n, p, [], range(0, n), j))
+    ranked = sorted(range(0, n), key=lambda j: index(m, n, p, range(0, n), 0, j))
 
     S = []
 
@@ -99,18 +98,15 @@ def lr(m, n, p, x):
         best = ranked.pop(0)
         pi[0], pi[best] = pi[best], pi[0]
 
-        for j in range(1, n-1):
-            pi_s, pi_u = pi[:j], pi[j:n]
-            u_xi = [index(m, n, p, pi_s, pi_u, u_j) for u_j in pi_u]
-            best = min(range(len(pi_u)), key=u_xi.__getitem__)
-
-            pi[j], pi[j + best] = pi[j + best], pi[j]
+        for k in range(1, n-1):
+            xi = [index(m, n, p, pi, k, j) for j in pi[k:n]]
+            best = min(range(n-k), key=xi.__getitem__)
+            pi[k], pi[k+best] = pi[k+best], pi[k]
 
         S.append(pi)
 
-    # 4. Retornar a sequência com o menor C_sum
-    S_C_sum = [total_completion_time(m, n, p, seq) for seq in S]
-    best = min(range(len(S_C_sum)), key=S_C_sum.__getitem__)
+    c_sum = [total_completion_time(m, n, p, seq) for seq in S]
+    best = min(range(x), key=c_sum.__getitem__)
     return S[best]
 
 
@@ -123,5 +119,7 @@ p = array([
         [16, 89, 49, 15, 89, 45, 60, 23, 57, 64,  7,  1, 63, 41, 63, 47, 26, 75, 77, 40],
         [66, 58, 31, 68, 78, 91, 13, 59, 49, 85, 85,  9, 39, 41, 56, 40, 54, 77, 51, 31],
         [58, 56, 20, 85, 53, 35, 53, 41, 69, 13, 86, 72,  8, 49, 47, 87, 58, 18, 68, 28]])
-print(lr(m, n, p, 1))
-print([2, 16, 8, 14, 13, 15, 5, 18, 12, 6, 11, 10, 7, 1, 0, 19, 3, 9, 4, 17])
+
+pi = lr(m, n, p, 1)
+print(pi)
+print(total_completion_time(m, n, p, pi))
