5912c5bec6b63680aa162f97f7b09a8788bdd8e5
F. Emerson <f@staticagent.com>
2020-04-25 23:26:51 -0300

    rvnd: Implementar vizinhança block swap

--- rvnd.py
+++ rvnd.py
@@ -7,8 +7,18 @@ def _l_block_insertion(l, x):
         for j in range(i):  # backward
             yield x[:j] + x[i:i+l] + x[j:i] + x[i+l:]
 
-        for j in range(i+l+1, len(x)+1):  #forward
+        for j in range(i+l+1, len(x)+1):  # forward
             yield x[:i] + x[i+l:j] + x[i:i+l] + x[j:]
+
+def _block_swap(l1, l2, x):
+    x = list(x)
+
+    for i in range(len(x)+1-l1):
+        for j in range(i-l2):  # backward
+            print(x[:j], x[i:i+l1], x[j+l2:i], x[j:j+l2], x[i+l1:])
+
+        for j in range(i+l1, len(x)+1-l2):  # forward
+            print(x[:i], x[j:j+l2], x[i+l1:j], x[i:i+l1], x[j+l2:])
 
 def rvnd(p, x):
     pi = x
