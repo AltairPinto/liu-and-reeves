46052d4fadf84e3f5b21e08676ff05ebaa6f4a8b
F. Emerson <f@staticagent.com>
2020-04-25 23:26:51 -0300

    Implementar vizinhança l-block insertion

--- rvnd.py
+++ rvnd.py
@@ -1,4 +1,18 @@
-def rvnd(m, n, p, x):
+import numpy as np
+from copy import copy
+
+def _l_block_insertion_forward(l, x):
+    x = list(x)
+    for i in range(len(x)-l):
+
+        # FIXME: the backward iteration is buggy
+        for j in range(0, i+l+1):  # backward
+            print(x[i:i+l], x[:i], x[i+l:j], x[j:])
+
+        for j in range(i+l+1, len(x)+1):  #forward
+            print(x[:i], x[i+l:j], x[i:i+l], x[j:])
+
+def rvnd(p, x):
     pi = x
 
     # Inicialize a lista de vizinhanças L
