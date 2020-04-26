5769f85474b2d0472dab725879c378c1175dfcbf
F. Emerson <f@staticagent.com>
2020-04-25 23:26:51 -0300

    Criar script inicial para RVND

---
+++ rvnd.py
@@ -0,0 +1,15 @@
+def rvnd(m, n, p, x):
+    pi = x
+
+    # Inicialize a lista de vizinhanças L
+
+    # Enquanto L tiver elementos:
+    #   Selecione uma vizinhança N dentro de L de forma aleatória
+    #   Encontre o melhor vizinho pi' de pi dentro de N
+    #   se total_completion_time(pi') < total_completion_time(pi):
+    #     pi = pi'
+    #     atualize L
+    #   senão
+    #     remova N de L
+
+    return pi
