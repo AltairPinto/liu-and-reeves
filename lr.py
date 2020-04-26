7b1f95c669705747e99ab2ff18f7eca6dfe0ba34
F. Emerson <f@staticagent.com>
2020-04-25 23:26:47 -0300

    lr: usar numpy

--- lr.py
+++ lr.py
@@ -1,108 +1,68 @@
 from typing import List, Sequence
+from numpy import array, zeros
 
 
 # Eq. 1
-def completion_time(
-        m: int,
-        n: int,
-        p: Sequence[int],
-        pi: Sequence[int],
-        i: int,
-        j: int) -> int:
-    assert i < m
-    assert j < len(pi)
-    assert len(pi) <= n
-    assert len(p) == m*n
-
+def completion_time(m, n, p, pi, i, j):
     # XXX(staticagent): As primeiras linha e coluna são inutilizadas, e estão
     # preenchidas com 0 para servir como dummies na comparação feita por max.
     # Existe um modo de utilizar esse conhecimento para diminuir ainda mais o
     # consumo de memória, mas, no momento, eu tenho que otimizar outras funções.
-    c = [0] * ((i+2) * (j+2))
+    c = zeros((i+2, j+2))
 
-    for machine in range(1, i+2):
-        for job in range(1, j+2):
-            lhs = c[(machine - 1)*(j+2) + job]
-            rhs = c[machine*(j+2) + job - 1]
+    for machine in range(1, c.shape[0]):
+        for job in range(1, c.shape[1]):
+            lhs = c[machine - 1][job]
+            rhs = c[machine][job - 1]
 
-            p_ij = p[(machine-1)*n + pi[job-1]]
+            p_ij = p[machine - 1][pi[job-1]]
 
-            c[machine*(j+2) + job] = max(lhs, rhs) + p_ij
+            c[machine][job] = max(lhs, rhs) + p_ij
 
-    return c[-1]
+    return c[-1][-1]
 
 # Eq. 2
-def total_completion_time(
-        m: int,
-        n: int,
-        p: Sequence[int],
-        pi: Sequence[int]) -> int:
-    assert len(p) == m*n
-    assert len(pi) == n
-
+def total_completion_time(m, n, p, pi):
     # XXX(staticagent): O mesmo comentário em completion_time aplica-se aqui.
-    c = [0] * ((m+1) * (n+1))
+    c = zeros((m+1, n+1))
 
-    for machine in range(1, m+1):
-        for job in range(1, n+1):
-            lhs = c[(machine - 1)*(n+1) + job]
-            rhs = c[machine*(n+1) + job - 1]
-            p_ij = p[(machine-1)*n + pi[job-1]]
+    for machine in range(1, c.shape[0]):
+        for job in range(1, c.shape[1]):
+            lhs = c[machine - 1][job]
+            rhs = c[machine][job - 1]
 
-            c[machine*(n+1) + job] = max(lhs, rhs) + p_ij
+            p_ij = p[machine - 1][pi[job-1]]
 
-    return c[-1]
+            c[machine][job] = max(lhs, rhs) + p_ij
+
+    return c[-1][-1]
 
 # Eq. 4
-def weight(
-        m: int,
-        n: int,
-        i: int,
-        k: int) -> float:
-    assert m >= 0
-    assert n > 2
-    assert i < m
-    assert k < n
-
+def weight(m, n, i, k):
     return m / (i + k*(m - i - 1)/(float)(n - 2)+1)
 
 
 # Eqs. 6 and 7
-def completion_time_if_next(
-        m: int,
-        n: int,
-        p: Sequence[int],
-        pi: Sequence[int],
-        i: int,
-        j: int) -> int:
-    assert len(p) == m*n
-    assert len(pi) < n
-    assert i < m
-    assert j < n
-
-    if i < 0:
-        return 0
-
+def completion_time_if_next(m, n, p, pi, i, j):
     # completion_time
-    c = [0] * ((i+2) * (len(pi)+1))
+    c = zeros((i+2, len(pi)+1))
 
-    k = len(pi)-1
-    for machine in range(1, i+2):
-        for job in range(1, k+2):
-            lhs = c[(machine - 1)*(k+2) + job]
-            rhs = c[machine*(k+2) + job - 1]
+    for machine in range(1, c.shape[0]):
+        for job in range(1, c.shape[1]):
+            lhs = c[machine - 1][job]
+            rhs = c[machine][job - 1]
 
-            p_ij = p[(machine-1)*n + pi[job-1]]
+            p_ij = p[machine - 1][pi[job-1]]
 
-            c[machine*(k+2) + job] = max(lhs, rhs) + p_ij
+            c[machine][job] = max(lhs, rhs) + p_ij
 
     # completion_time_if_next
-    c_next = [0] * (i+2)
-    for machine in range(1, i+2):
+    c_next = zeros((i+2))
+    for machine in range(1, c_next.shape[0]):
         rhs = c_next[machine - 1]
-        lhs = c[(machine)*(k+2) + k+1]
+        lhs = c[machine][-1]
 
-        p_ij = p[(machine-1)*n + j]
+        p_ij = p[machine - 1][j]
 
         c_next[machine] = max(lhs, rhs) + p_ij
 
@@ -110,62 +70,50 @@ def completion_time_if_next(
 
 
 # Eq. 3
-def weighted_total_machine_idle_time(
-        m: int,
-        n: int,
-        p: Sequence[int],
-        pi: Sequence[int],
-        j: int) -> float:
-    assert len(p) == m*n
-    assert len(pi) < n
-    assert j < n
+def weighted_total_machine_idle_time(m, n, p, pi, j):
+    # completion_time
+    c = zeros((m+1, len(pi)+1))
+
+    for machine in range(1, c.shape[0]):
+        for job in range(1, c.shape[1]):
+            lhs = c[machine - 1][job]
+            rhs = c[machine][job - 1]
+
+            p_ij = p[machine - 1][pi[job-1]]
+
+            c[machine][job] = max(lhs, rhs) + p_ij
+
+    # completion_time_if_next
+    c_next = zeros((c.shape[0]))
+    for machine in range(1, c_next.shape[0]):
+        rhs = c_next[machine - 1]
+        lhs = c[machine][-1]
+
+        p_ij = p[machine - 1][j]
+
+        c_next[machine] = max(lhs, rhs) + p_ij
 
     it = 0.
 
     for i in range(1, m):
-        idle_time = max(completion_time_if_next(m, n, p, pi, i-1, j) - completion_time(m, n, p, pi, i, len(pi)-1), 0)
+        idle_time = max(c_next[i] - c[i+1][-1], 0)
         it += weight(m, n, i, len(pi)) * idle_time
 
     return it
 
 
 # Eq. 5
-def artificial_processing_time(
-        m: int,
-        n: int,
-        p: Sequence[int],
-        u: Sequence[int],
-        i: int,
-        j: int) -> float:
-    assert len(p) == m*n
-    assert len(u) <= n
-    assert len(u) > 1
-    assert i < m
-    assert j < n
-
+def artificial_processing_time(m, n, p, u, i, j):
     p_a = 0.
     for q in u:
-        p_a += p[i*n + q]
+        p_a += p[i][q]
 
-    p_a -= p[i*n + j]
+    p_a -= p[i][j]
 
     return p_a / (len(u) - 1)
 
 # Eqs. 8 and 9
-def artificial_completion_time(
-        m: int,
-        n: int,
-        p: Sequence[int],
-        pi: Sequence[int],
-        u: Sequence[int],
-        i: int,
-        j: int) -> float:
-    assert len(p) == m*n
-    assert len(pi)+len(u) == n
-    assert len(u) > 1
-    assert i < m
-    assert j < n
-
+def artificial_completion_time(m, n, p, pi, u, i, j):
     if i < 0:
         return 0.
 
@@ -176,49 +124,21 @@ def artificial_completion_time(
 
 
 # Eq. 10
-def artificial_total_completion_time(
-        m: int,
-        n: int,
-        p: Sequence[int],
-        pi: Sequence[int],
-        u: Sequence[int],
-        j: int) -> float:
-    assert len(p) == m*n
-    assert len(pi)+len(u) == n
-    assert len(u) > 1
-    assert j < n
-
+def artificial_total_completion_time(m, n, p, pi, u, j):
     c_j = completion_time_if_next(m, n, p, pi, m-1, j)
     c_a = artificial_completion_time(m, n, p, pi, u, m-1, j)
     return c_j + c_a
 
 
 # Eq. 11
-def index(
-        m: int,
-        n: int,
-        p: Sequence[int],
-        pi: Sequence[int],
-        u: Sequence[int],
-        j: int) -> float:
+def index(m, n, p, pi, u, j):
     it = weighted_total_machine_idle_time(m, n, p, pi, j)
     at = artificial_total_completion_time(m, n, p, pi, u, j)
     return (len(u) - 2)*it + at
 
 
-def lr(
-        m: int,
-        n: int,
-        p: Sequence[int],
-        x: int) -> List[int]:
-    assert m > 0
-    assert n > 2
-    assert len(p) == m*n
-    assert x >= 1
-    assert x <= n
-
-    pi_base = list(range(0, n))
-    ranked = sorted(pi_base, key=lambda j: index(m, n, p, pi_base[:0], pi_base[0:n], j))
+def lr(m, n, p, x):
+    ranked = sorted(range(0, n), key=lambda j: index(m, n, p, [], range(0, n), j))
 
     S = []
 
@@ -246,10 +166,11 @@ def lr(
 # dados para teste - ta001
 m = 5
 n = 20
-p = [
-        54, 83, 15, 71, 77, 36, 53, 38, 27, 87, 76, 91, 14, 29, 12, 77, 32, 87, 68, 94,
-        79,  3, 11, 99, 56, 70, 99, 60,  5, 56,  3, 61, 73, 75, 47, 14, 21, 86,  5, 77,
-        16, 89, 49, 15, 89, 45, 60, 23, 57, 64,  7,  1, 63, 41, 63, 47, 26, 75, 77, 40,
-        66, 58, 31, 68, 78, 91, 13, 59, 49, 85, 85,  9, 39, 41, 56, 40, 54, 77, 51, 31,
-        58, 56, 20, 85, 53, 35, 53, 41, 69, 13, 86, 72,  8, 49, 47, 87, 58, 18, 68, 28]
-print(lr(m, n, p, 1) == [2, 16, 8, 14, 13, 15, 5, 18, 12, 6, 11, 10, 7, 1, 0, 19, 3, 9, 4, 17])
+p = array([
+        [54, 83, 15, 71, 77, 36, 53, 38, 27, 87, 76, 91, 14, 29, 12, 77, 32, 87, 68, 94],
+        [79,  3, 11, 99, 56, 70, 99, 60,  5, 56,  3, 61, 73, 75, 47, 14, 21, 86,  5, 77],
+        [16, 89, 49, 15, 89, 45, 60, 23, 57, 64,  7,  1, 63, 41, 63, 47, 26, 75, 77, 40],
+        [66, 58, 31, 68, 78, 91, 13, 59, 49, 85, 85,  9, 39, 41, 56, 40, 54, 77, 51, 31],
+        [58, 56, 20, 85, 53, 35, 53, 41, 69, 13, 86, 72,  8, 49, 47, 87, 58, 18, 68, 28]])
+print(lr(m, n, p, 1))
+print([2, 16, 8, 14, 13, 15, 5, 18, 12, 6, 11, 10, 7, 1, 0, 19, 3, 9, 4, 17])
